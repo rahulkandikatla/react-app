e39715ff518cb9bea31ae9843a3a2d5f7d4ce1f6
rahulkandikatla <rahulkandikatla33@gmail.com>
2020-05-20 21:46:15 +0530

    practice

---
+++ src/components/common/NoDataView/styledComponents.js
@@ -0,0 +1,10 @@
+import styled from "./node_modules/@emotion/styled";
+import tw from "./node_modules/tailwind.macro";
+
+export const NoDataViewContainer = styled.div`
+  ${tw`text-center m-4`}
+`;
+
+export const NoDataViewText = styled.p`
+  ${tw`text-xl`}
+`;
